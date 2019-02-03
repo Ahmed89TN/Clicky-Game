3fb6b9b5cbe3c2b2ad0158cbdff4f18032693450
Ahmed89TN <ahmedbenothmen045@gmail.com>
2019-02-03 13:02:05 -0800

    Updates

---
+++ precache-manifest.c0924e8185e0e168c4eb01a6841f96e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "8e5401196d75701f03d1",
+    "url": "/clicky-game/static/js/main.8e540119.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "8e5401196d75701f03d1",
+    "url": "/clicky-game/static/css/main.22ded542.chunk.css"
+  },
+  {
+    "revision": "6004dc41e8674b7f0548af3fed25e8aa",
+    "url": "/clicky-game/index.html"
+  }
+];
