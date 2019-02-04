6f5db9bc474cd57355bd3abe01c00d088009f1e2
Ahmed89TN <ahmedbenothmen045@gmail.com>
2019-02-03 21:53:37 -0800

    font fix

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Nav = props => (
   <nav>
     <ul>
-      <li className="brand animated lightSpeedIn">
+      <li>
         <a href="/clicky-game/">{props.title}</a>
       </li>
 
